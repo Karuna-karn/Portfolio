a085668e6f3ccafbfbf2f0405631dd7ce82dff1b
Karuna karn <kumarikoshiki@gmail.com>
2026-04-26 07:13:48 +0530

    final portfolio

--- scipt.js
+++ scipt.js
@@ -1,2 +1,29 @@
-// You can add animations or interactive features here later.
-console.log("Portfolio loaded successfully!");
+
+// Typing effect
+const text = "Karuna Karn";
+let i = 0;
+
+function type() {
+  if (i < text.length) {
+    document.getElementById("typing").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(type, 100);
+  }
+}
+type();
+
+// Scroll
+function scrollToProjects() {
+  document.getElementById("projects").scrollIntoView({
+    behavior: "smooth"
+  });
+}
+
+// Progress bar
+window.onscroll = () => {
+  let scroll = document.documentElement.scrollTop;
+  let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+  document.getElementById("progress").style.width =
+    (scroll / height) * 100 + "%";
+};
