af65649f7cc65c9374b98a9f73fc0fcb05e46b09
Karuna karn <kumarikoshiki@gmail.com>
2026-06-19 10:18:58 +0530

    Update Portfolio changes

--- script.js
+++ script.js
@@ -5,6 +5,20 @@ function openLink(url){
 /* typing effect */
 const text = "Frontend Developer | MERN Stack Learner";
 let i = 0;
+const toggleBtn = document.getElementById("theme-toggle");
+
+toggleBtn.addEventListener("click", () => {
+
+    document.body.classList.toggle("dark");
+
+    if(document.body.classList.contains("dark")){
+        toggleBtn.innerHTML = "☀️";
+    }
+    else{
+        toggleBtn.innerHTML = "🌙";
+    }
+
+});
 
 function type(){
   if(i < text.length){
